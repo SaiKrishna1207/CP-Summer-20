c61c9d707a7d5bf2be5f31875e18261ffb4e7671
CyberSkam <saikrishna001207@gmail.com>
2020-04-01 20:00:28 +0530

    Partytime

---
+++ Day-6/Party.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solve(unordered_map<int,vector<int>>& m, int t)
+{
+    int j, h, max1 = 0;
+    vector<int> a = m[t];
+    if(a.size() == 0)
+        return 0;
+    for(j = 0; j < a.size(); j++)
+    {
+        h = solve(m, a[j]);
+        if(h > max1)
+            max1 = h;
+    }
+    return max1 + 1;
+}
+
+int main()
+{
+    int n, i, max = 0, x;
+    cin >> n;
+    int p[n+1];
+    unordered_map <int, vector<int>> m;
+    for(i = 1;i <= n; i++)
+    {
+        cin >> p[i];
+        m[p[i]].push_back(i);
+    }
+    vector<int> roots = m[-1];
+    for(i = 0;i < roots.size(); i++)
+    {
+        x = solve(m, roots[i]);
+        if(x > max)
+            max = x;
+    }
+    //We keep getting 1 less because levels start as 0. Hence we increment max.
+    max++;
+    cout << max << endl;
+    return 0;
+}
