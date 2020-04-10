87d7e80879639719e0cbb183dc120d319be99828
CyberSkam <saikrishna001207@gmail.com>
2020-04-10 16:55:49 +0530

    Took a while

--- Day-14/skyscrapers1.cpp
+++ Day-14/skyscrapers1.cpp
@@ -6,40 +6,40 @@ int main()
 {
 	long long n;
     cin>>n;
-    long long max[n], i, maxm=0, ele, sum=0, j, x;
+    long long max[n], i, j, ind, h, maxm = 0, sum = 0;
 	for(i = 0;i < n; i++)
         cin >> max[i];
     vector<long long> arr[n];
 	for(i = 0;i < n; i++)
 	{
-		ele = max[i];
-		sum += ele;
+		h = max[i];
+		sum += h;
 		for(j = i - 1;j >= 0; j--)
 		{
-            if(max[j] < ele)
-                ele = max[j];
-            sum += ele;
-            arr[i].push_back(ele);
+            if(max[j] < h)
+                h = max[j];
+            sum += h;
+            arr[i].push_back(h);
 		}
-		reverse(arr[i].begin(),arr[i].end());
-		ele = max[i];
-		arr[i].push_back(ele);
+		reverse(arr[i].begin(), arr[i].end());
+		h = max[i];
+		arr[i].push_back(h);
 		for(j = i + 1;j < n;j++)
 		{
-            if(max[j] < ele)
-                ele = max[j];
-            sum += ele;
-            arr[i].push_back(ele);
+            if(max[j] < h)
+                h = max[j];
+            sum += h;
+            arr[i].push_back(h);
 		}
 		if(sum > maxm)
         {
             maxm = sum;
-            x = i;
+            ind = i;
         }
 		sum = 0;
 	}
-	for(i = 0;i < arr[x].size();i++)
-        cout << arr[x][i] <<" "; 
+	for(i = 0;i < arr[ind].size();i++)
+        cout << arr[ind][i] <<" "; 
     
     return 0;
 }
