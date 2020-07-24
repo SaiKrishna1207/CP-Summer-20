9eeb49417bce5867c45882e2e6ae57ecf0db1778
Sai Krishna Anand <saikrishna001207@gmail.com>
2020-07-24 13:56:14 +0530

    Optimized memo

--- Week-10/Day-64/longestPathOpt.cpp
+++ Week-10/Day-64/longestPathOpt.cpp
@@ -1,5 +1,7 @@
 class Solution {
 public:
+    int ans = 0;
+
     int dfs(vector<vector<int>>& matrix, vector<vector<int>>& dp, int i, int j) {
         if (dp[i][j] > 0) 
             return dp[i][j];
@@ -11,6 +13,7 @@ public:
         
         int mx = max({l, r, u, d});
         dp[i][j] = mx + 1;
+        ans = max(ans, dp[i][j]);
         
         return dp[i][j];
     }
@@ -20,7 +23,6 @@ public:
             return INT_MIN;
         return matrix[i][j];
     }
-    
     
     int longestIncreasingPath(vector<vector<int>>& matrix) {
         if (matrix.size() == 0 || matrix[0].size() == 0) 
@@ -33,11 +35,6 @@ public:
             for (j = 0; j < dp[i].size(); j++) {
                 dfs(matrix, dp, i, j);
             }
-        }
-        
-        for (i = 0; i < dp.size(); i++) {
-            for (j = 0; j < dp[i].size(); j++) 
-                ans = max(ans, dp[i][j]);
         }
         return ans;
     }
