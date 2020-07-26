00bafbc2700bd6bbcc01b271ceea288d2b80e87e
Sai Krishna Anand <saikrishna001207@gmail.com>
2020-07-26 11:15:12 +0530

    My dp

--- Week-10/Day-65/regEx.cpp
+++ Week-10/Day-65/regEx.cpp
@@ -1,34 +1,37 @@
-bool dfs(vector<vector<bool>> &dp, const string &A, const string &B, int i, int j) {
-    if(i < 0 && j < 0) 
+int Solution::isMatch(const string s, const string B) {
+    int m = s.length(), n = B.length(), i, j, k;
+    if(!m && !n)
         return true;
-    if(j < 0) 
+    if(!m || !n)
         return false;
-    if(i < 0) {
-        while(j >= 0) {
-            if(B[j] != '*') 
-                return false;
-            j--;
+    int l = 0;
+    bool kk = true;
+    string p = B;
+    for(i = 0;i < p.length(); i++){
+        if(p[i] == '*'){
+            if(kk){
+                p[l++] = p[i];
+                kk = false;
+            }
         }
-        return true;
+        else{
+            p[l++] = p[i];
+            kk = true;
+        }
     }
-    if(dp[i][j] == false) 
-        return false;
-    
-    bool check = false;
-    
-    if(B[j] == '*') 
-        check = dfs(dp, A, B, i-1, j) || dfs(dp, A, B, i, j-1);
-    else if(B[j] == A[i] || B[j] == '?') 
-        check = dfs(dp, A, B, i-1, j-1);
-    
-    if(check) 
-        return true;
-    return dp[i][j] = false;
-} 
-int Solution::isMatch(const string A, const string B) {
-    int i, j, k, m = A.length(), n = B.length();
-    if(m == 0 || n == 0) 
-        return 0;
-    vector<vector<bool>> dp(m, vector<bool>(n, true));
-    return dfs(dp, A, B, m - 1, n - 1);
+    vector<vector<bool>> dp(m + 1, vector<bool>(l + 1, false));
+    dp[0][0] = true;
+    if(p[0] == '*')
+        dp[0][1] = true;
+    for(i = 1; i <= m; i++){
+        for(j = 1; j <= l; j++){
+            if(p[j - 1] == s[i - 1] || p[j - 1] == '?')
+                dp[i][j] = dp[i - 1][j - 1];
+            else if(p[j - 1] == '*')
+                dp[i][j] = dp[i - 1][j] | dp[i][j - 1];
+            else
+                dp[i][j] = false;
+        }
+    }
+    return dp[m][l];
 }
