5dd9062ea98a356b933b9ca7801182f3212cabac
Sai Krishna Anand <saikrishna001207@gmail.com>
2020-07-26 13:57:56 +0530

    DFS lyf

--- Week-10/Day-66/Dist.cpp
+++ Week-10/Day-66/Dist.cpp
@@ -22,31 +22,26 @@ public:
     }
     
     vector<int> dfs(TreeNode* root, int d) {
-        vector<int> res(11);
+        vector<int> t(11);
         if(!root) 
-            return res;
+            return t;
         
         if(!root->left && !root->right) {
-            res[1] = 1;
-            return res;
+            t[1] = 1;
+            return t;
         }
         int i, j;
-        
         vector<int> l = dfs(root->left, d);
         vector<int> r = dfs(root->right, d);
     
-        for(int i = 1; i <= 10; i++) {
+        for(i = 1; i <= 10; i++) {
             if(l[i]) {
                 for(j = 1; j <= d - i; j++) 
                     ans += l[i] * r[j]; 
             }
         }
-
-        //for my parent node, set number of node at distance+1 position
-        //discard those whose distance is already exceeding the limit
-        for(i = 0; i < d; i++) {
-            res[i + 1] += l[i] + r[i];
-        }
+        for(i = 0; i < d; i++) 
+            t[i + 1] += l[i] + r[i];
         
         return res;
     }
